2fd003a66d4463b07edec243a50f1c930ac5f6c2
Jack Dagley <jack.dagley77@gmail.com>
2017-06-05 09:22:58 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+// targets the div with an id of 'events'
+new Vue({
+	el: '#events',
+
+	// register any values or collections that hold data for the application
+	data: {},
+
+	// anything in ready will run when the application loads
+	ready: function() {},
+
+	methods: {}
+})
+
