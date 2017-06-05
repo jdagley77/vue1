ca2ddeaf2580e951767fe9297893a25a158ad9d6
Jack Dagley <jack.dagley77@gmail.com>
2017-06-05 14:00:35 -0700

    Add delete method and event display

--- app.js
+++ app.js
@@ -1,53 +1,67 @@
+// var Vue = require('vue');
 
 // targets the div with an id of 'events'
 new Vue({
-	el: '#events',
+	el: "#events",
 
 	// register any values or collections that hold data for the application
 	data: {
-		event: {name: '', description: '', date: ''},
-		events: []
+	  event: { name: '', description: '', date: '' },
+	  events: []
 	},
 
-	// anything in ready will run when the application loads
+	// Anything within the ready function will run when the application loads
 	ready: function() {
-		// when the application loads, we want to call on this method to initialize some data
-		this.fetchEvents();
+	  // When the application loads, we want to call the method that initializes
+	  // some data
+	  this.fetchEvents();
 	},
-	// methods we want to use in our application are registered here
+
+	// Methods we want to use in our application are registered here
 	methods: {
-		fetchEvents: function() {
-			// when the application loads, we want to call the method that initializes some data
-			var events = [
-				{
-					id: 1,
-					name: 'TIFF',
-					description: 'Toronto International Film Festival',
-					date: '2015-10-02'
-				},
-				{
-					id: 2,
-					name: 'The Martian Premiere',
-					description: 'The Martian comes to theatres.',
-					date: '2015-10-02'
-				},
-				{
-					id: 3,
-					name: 'SXSW',
-					description: 'Music, film and interactive festival in Austin, TX'
-					date: '2016-03-11'
-				}
-			];
-			// $set is a convenience method provided by Vue that is similar to pushing an event onto an array
-			this.$set('events', events);
-		},
-		// adds an event to existing events array
-		addEvent: function() {
-			if(this.event.name) {
-				this.events.push(this.event)
-				this.event = { name: '', description: '', date: ''};
+
+	  // We dedicate a method to retrieving and setting some data
+	  fetchEvents: function() {
+	    var events = [
+	      {
+	        id: 1,
+	        name: 'TIFF',
+	        description: 'Toronto International Film Festival',
+	        date: '2015-09-10'
+	      },
+	      {
+	        id: 2,
+	        name: 'The Martian Premiere',
+	        description: 'The Martian comes to theatres.',
+	        date: '2015-10-02'
+	      },
+	      {
+	        id: 3,
+	        name: 'SXSW',
+	        description: 'Music, film and interactive festival in Austin, TX.',
+	        date: '2016-03-11'
+	      }
+	    ];
+	    // $set is a convenience method provided by Vue that is similar to pushing
+	    // data onto an array
+	    this.$set('events', events);
+	  },
+
+	  // Adds an event to the existing events array
+	  addEvent: function() {
+	    if(this.event.name) {
+	      this.events.push(this.event);
+	      this.event = { name: '', description: '', date: '' };
+	    }
+	  },
+
+
+		deleteEvent: function() {
+			if(confirm("Are you sure you want to delete this event?")) {
+				this.events.$remove(index);
 			}
 		}
+
 	}
-})
+});
 
